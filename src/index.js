b370b2143f818a5ef82d72306286da509f5ea7ff
Sam <deleonsam87@gmail.com>
2021-08-05 00:24:53 -0500

    Created css skeleton and created a bundle using webpack

---
+++ src/index.js
@@ -0,0 +1,20 @@
+"use strict";
+import "./style.css";
+
+console.log("Yo!");
+console.log("Please dont take to long");
+
+// Object controling Program IFFIE
+class Program {
+  constructor() {}
+
+  // Create a toto object (takes info from the popup)
+
+  // show popup to create a todo object
+
+  // colect info from the popup
+
+  // Containe Todo objects
+
+  // Display Toto todo container
+}
